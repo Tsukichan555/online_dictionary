e74741122cb482c114e91fa21ac9659888159e20
Natsuki <Tsukichan555.dev@gmail.com>
2022-10-30 20:17:10 +0900

    配置を調整

--- main.py
+++ main.py
@@ -1,10 +1,9 @@
 import tkinter
+from tkinter.font import NORMAL
 import requests
 from bs4 import BeautifulSoup
 import urllib.parse
-from tkinter import scrolledtext
-import pprint
-
+from tkinter import DISABLED, scrolledtext
 
 def search(lang,param):
     param = urllib.parse.quote(param) #urlエンコード
@@ -33,7 +32,7 @@ def search(lang,param):
         soup = BeautifulSoup(html_txt_de,'html.parser')
         integrated = ''
 
-        try:
+        try:#リダイレクトする場合
             redirect_url = soup.find('p',{'data-orgtag':'meaning'}).find('a')['href']
             original_word = soup.find('p',{'data-orgtag':'meaning'}).text
             integrated += original_word + '\n\n'
@@ -41,7 +40,8 @@ def search(lang,param):
             soup = BeautifulSoup(html_txt_de,'html.parser')
         except TypeError:
             pass
-
+        
+        #結果
         definition_elements = soup.find_all('p',{'data-orgtag':'meaning'})
         result=[]
         for el in definition_elements:
@@ -58,45 +58,54 @@ def search(lang,param):
 
 
 class Application(tkinter.Frame):
-    def __init__(self,root=None):
-        super().__init__(root,width=900,height=500,borderwidth=1,relief='groove')
-        self.root = root
+    def __init__(self,master=None):
+        super().__init__(master,borderwidth=1,relief='flat') #width=900,height=500,
         self.pack()
-        self.pack_propagate(0)
+        #self.pack_propagate(0)
         self.create_widgets()
 
     def create_widgets(self):
-        default=('Helvetica',18)
+        default=('游明朝',18)
 
-        self.text_box = tkinter.Entry(self,font=default,width=30)
-        self.text_box.pack(fill='x',padx=100)
+        #検索バー
+        self.searchbar = tkinter.Frame(self,relief=tkinter.GROOVE)
+        self.text_box = tkinter.Entry(self.searchbar,font=default,width=30)
+        self.button = tkinter.Button(self.searchbar,text='検索(E)',font=default,command=self.submit,width=10)#
 
+        #言語選択
         items = ['ドイツ語','英語']        
-        self.sp = tkinter.Spinbox(self,font=default,state='readonly',values=items)
-        self.sp.pack()
+        self.selection = tkinter.Spinbox(self,font=default,state='readonly',values=items,width=20)
 
-        button = tkinter.Button(self,text='検索',font=default,command=self.submit)
-        button.pack(fill='x',padx=130)
-        
+        #検索結果領域
         self.view = scrolledtext.ScrolledText(self,font=default)
         self.view.insert('1.0','単語を入力してボタンを押すと、ここに検索結果が表示されます。')
-        self.view.pack()
-        
+        self.view.config(state=DISABLED)
+
         #エンターで検索
         self.text_box.bind('<Return>',self.submit)
         #Entryにフォーカス
         self.text_box.focus_set()
+        #配置
+        self.searchbar.pack()
+        self.selection.pack()
+        self.view.pack()
+        self.text_box.pack(side=tkinter.LEFT)
+        self.button.pack(side=tkinter.LEFT)
+
+        
 
     def submit(self,event):
-        lang = self.sp.get()
+        lang = self.selection.get()
         text = self.text_box.get()
 
         textv = search(lang,text)
+        self.view.config(state=NORMAL)
         self.view.delete(1.0,tkinter.END)
         self.view.insert('1.0',textv)
+        self.view.config(state=DISABLED)
 
 root = tkinter.Tk()
-root.title('調べてえら～い！')
+root.title('LATIN-GRAECUM DICTIONARIUM')
 root.geometry('500x300')
-app = Application(root=root)
+app = Application(master=root)
 app.mainloop()
